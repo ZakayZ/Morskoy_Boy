4207442c89a593f39eac5bd0d47165245ee9e88d
ZakayZ <novikov.artemii@phystech.edu>
2022-03-12 14:57:22 +0300

    add usings

--- includes.h
+++ includes.h
@@ -1,10 +1,14 @@
-#ifndef SHIPS_MORSKOY_BOY_INCLUDES_H_
-#define SHIPS_MORSKOY_BOY_INCLUDES_H_
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <vector>
+#include <iostream>
 
-#include "Ships//Ship.h"
+#include "Ships/Ship.h"
 #include "Weapons/Weapon.h"
 #include "Player/Player.h"
 #include "GameMaster/GameMaster.h"
 #include "Field/Field.h"
 
-#endif //SHIPS_MORSKOY_BOY_INCLUDES_H_
+using std::vector;
+using std::cout;
+using std::cin;
