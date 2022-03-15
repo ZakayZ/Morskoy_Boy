7327310bbd6378d9d9e5730938252038de08291f
Dionysour <ismagilov.dv@phystech.edu>
2022-03-15 22:48:29 +0300

    Projectile added

--- Weapons/Weapon.cpp
+++ Weapons/Weapon.cpp
@@ -1,5 +1,1 @@
-//
-// Created by Artem Novikov on 09.03.2022.
-//
-
 #include "Weapon.h"
--- Weapons/Weapon.h
+++ Weapons/Weapon.h
@@ -6,7 +6,14 @@
 #define SHIPS_MORSKOY_BOY_WEAPONS_WEAPON_H_
 
 class Weapon {
-
+ public:
+  virtual Fire();
+  virtual Reload();
+  virtual
+ private:
+  size_t reload_counter_;
+  vector<vector<uint64_t>> damage_kernel_;
+  size_t time_;
 };
 
 #endif //SHIPS_MORSKOY_BOY_WEAPONS_WEAPON_H_
