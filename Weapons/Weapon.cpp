e9f8885e69510d67b6c23daf244252b9e71a769f
Dionysour <ismagilov.dv@phystech.edu>
2022-04-11 21:47:38 +0300

    small fixes

--- Weapons/Weapon.cpp
+++ Weapons/Weapon.cpp
@@ -11,6 +11,7 @@ bool Weapon::IsReadyToFire() const {
 }
 
 std::shared_ptr<Projectile> Gun::Fire(Coords cord) {
+  reload_counter_ = kTimeToReload_;
   auto ptr = std::make_shared<GunProjectile>(cord, kTimeToFly_);
   return ptr;
 }
@@ -20,14 +21,16 @@ const size_t Gun::kTimeToFly_ = 1;
 const size_t Gun::kTimeToReload_ = 1;
 
 std::shared_ptr<Projectile> Mortar::Fire(Coords cord) {
+  reload_counter_ = kTimeToReload_;
   return std::make_shared<MortarProjectile>(cord, kTimeToFly_);
 }
 
 const size_t Mortar::kTimeToFly_ = 2;
 
-const size_t Mortar::kTimeToReload = 2;
+const size_t Mortar::kTimeToReload_ = 2;
 
 std::shared_ptr<Projectile> FlareGun::Fire(Coords cord) {
+  reload_counter_ = kTimeToReload_;
   return std::make_shared<MortarProjectile>(cord, kTimeToFly_);
 }
 
